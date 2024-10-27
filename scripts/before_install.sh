647761583c1f168e6f45c4ad1e6887ee33299a03
ysam020 <sameery.020@gmail.com>
2024-10-27 09:08:32 +0530

    updated scripts

--- scripts/before_install.sh
+++ scripts/before_install.sh
@@ -4,7 +4,7 @@ curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.34.0/install.sh | bash
 . ~/.nvm/nvm.sh
 nvm install node
 # Create our working directory if it doesn't exist
-DIR="/home/ec2-user/cicd/pm-cicd/server"
+DIR="/home/ec2-user/cicd/pm-cicd"
 if [ -d "$DIR" ]; then
   echo "${DIR} exists"
 else
