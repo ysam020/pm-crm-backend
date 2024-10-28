9601a292e4b6896a5ea19e235c6b788203d436ec
ysam020 <sameery.020@gmail.com>
2024-10-28 14:59:34 +0530

    using optimistic concurrency in user schema

--- model/userModel.mjs
+++ model/userModel.mjs
@@ -25,164 +25,167 @@ const sessionSchema = new Schema({
 });
 
 // User schema
-const userSchema = new Schema({
-  username: {
-    type: String,
-    required: true,
-    unique: true,
-  },
-  password: {
-    type: String,
-    required: true,
-  },
-  role: { type: String },
-  modules: [
-    {
+const userSchema = new Schema(
+  {
+    username: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    role: { type: String },
+    modules: [
+      {
+        type: String,
+      },
+    ],
+    ////////////////////////////////////////////////////////////////// Onboarding
+    first_name: {
       type: String,
     },
-  ],
-  ////////////////////////////////////////////////////////////////// Onboarding
-  first_name: {
-    type: String,
-  },
-  middle_name: {
-    type: String,
-  },
-  last_name: {
-    type: String,
-  },
-  email: {
-    type: String,
-    unique: true,
-  },
-  employment_type: { type: String },
-  skill: {
-    type: String,
-  },
-  company_policy_visited: {
-    type: String,
-  },
-  employee_photo: {
-    type: String,
-  },
-  resume: { type: String },
-  address_proof: { type: String },
+    middle_name: {
+      type: String,
+    },
+    last_name: {
+      type: String,
+    },
+    email: {
+      type: String,
+      unique: true,
+    },
+    employment_type: { type: String },
+    skill: {
+      type: String,
+    },
+    company_policy_visited: {
+      type: String,
+    },
+    employee_photo: {
+      type: String,
+    },
+    resume: { type: String },
+    address_proof: { type: String },
 
-  ////////////////////////////////////////////////////////////////// KYC
-  designation: {
-    type: String,
-  },
-  department: {
-    type: String,
-  },
-  joining_date: {
-    type: String,
-  },
-  permanent_address_line_1: {
-    type: String,
-  },
-  permanent_address_line_2: {
-    type: String,
-  },
-  permanent_address_city: {
-    type: String,
-  },
-  permanent_address_state: {
-    type: String,
-  },
-  permanent_address_pincode: {
-    type: String,
-  },
-  communication_address_line_1: {
-    type: String,
-  },
-  communication_address_line_2: {
-    type: String,
-  },
-  communication_address_city: {
-    type: String,
-  },
-  communication_address_state: {
-    type: String,
-  },
-  communication_address_pincode: {
-    type: String,
-  },
-  official_email: {
-    type: String,
-  },
-  dob: {
-    type: String,
-  },
-  mobile: {
-    type: String,
-  },
-  blood_group: {
-    type: String,
-  },
-  highest_qualification: {
-    type: String,
-  },
-  aadhar_no: {
-    type: String,
-  },
-  aadhar_photo_front: {
-    type: String,
-  },
-  aadhar_photo_back: {
-    type: String,
-  },
-  pan_no: {
-    type: String,
-  },
-  pan_photo: {
-    type: String,
-  },
-  pf_no: {
-    type: String,
-  },
-  esic_no: {
-    type: String,
-  },
-  insurance_status: [
-    {
+    ////////////////////////////////////////////////////////////////// KYC
+    designation: {
       type: String,
     },
-  ],
-  bank_account_no: {
-    type: String,
+    department: {
+      type: String,
+    },
+    joining_date: {
+      type: String,
+    },
+    permanent_address_line_1: {
+      type: String,
+    },
+    permanent_address_line_2: {
+      type: String,
+    },
+    permanent_address_city: {
+      type: String,
+    },
+    permanent_address_state: {
+      type: String,
+    },
+    permanent_address_pincode: {
+      type: String,
+    },
+    communication_address_line_1: {
+      type: String,
+    },
+    communication_address_line_2: {
+      type: String,
+    },
+    communication_address_city: {
+      type: String,
+    },
+    communication_address_state: {
+      type: String,
+    },
+    communication_address_pincode: {
+      type: String,
+    },
+    official_email: {
+      type: String,
+    },
+    dob: {
+      type: String,
+    },
+    mobile: {
+      type: String,
+    },
+    blood_group: {
+      type: String,
+    },
+    highest_qualification: {
+      type: String,
+    },
+    aadhar_no: {
+      type: String,
+    },
+    aadhar_photo_front: {
+      type: String,
+    },
+    aadhar_photo_back: {
+      type: String,
+    },
+    pan_no: {
+      type: String,
+    },
+    pan_photo: {
+      type: String,
+    },
+    pf_no: {
+      type: String,
+    },
+    esic_no: {
+      type: String,
+    },
+    insurance_status: [
+      {
+        type: String,
+      },
+    ],
+    bank_account_no: {
+      type: String,
+    },
+    bank_name: {
+      type: String,
+    },
+    ifsc_code: {
+      type: String,
+    },
+    kyc_date: { type: String },
+    kyc_approval: {
+      type: String,
+    },
+    // Password reset fields
+    resetPasswordOTP: {
+      type: String,
+    },
+    resetPasswordExpires: {
+      type: Date,
+    },
+    sessions: [sessionSchema],
+    // 2FA fields
+    twoFactorSecret: {
+      type: String,
+    },
+    // Rate-limiting fields for login attempts
+    failedLoginAttempts: { type: Number, default: 0 },
+    firstFailedLoginAt: { type: Date },
+    isBlocked: { type: Boolean, default: false },
+    blockedUntil: { type: Date },
+    backupCodes: {
+      type: [String], // Array to store backup codes
+    },
   },
-  bank_name: {
-    type: String,
-  },
-  ifsc_code: {
-    type: String,
-  },
-  kyc_date: { type: String },
-  kyc_approval: {
-    type: String,
-  },
-  // Password reset fields
-  resetPasswordOTP: {
-    type: String,
-  },
-  resetPasswordExpires: {
-    type: Date,
-  },
-  sessions: [sessionSchema],
-  // 2FA fields
-  twoFactorSecret: {
-    type: String,
-  },
-  // Rate-limiting fields for login attempts
-  failedLoginAttempts: { type: Number, default: 0 },
-  firstFailedLoginAt: { type: Date },
-  isBlocked: { type: Boolean, default: false },
-  blockedUntil: { type: Date },
-  backupCodes: {
-    type: [String], // Array to store backup codes
-  },
-});
+  { optimisticConcurrency: true }
+);
 
 // Method to generate backup codes
 userSchema.methods.generateBackupCodes = function (numCodes = 10) {
