4a5562594ac5c9274a23252fe7f314489da4575a
ysam020 <sameery.020@gmail.com>
2024-10-29 22:44:34 +0530

    removed webauthn

--- utils/generateChallenge.mjs
+++
@@ -1,8 +0,0 @@
-import crypto from "crypto";
-import base64url from "base64url";
-
-// Function to generate a random challenge
-export function generateChallenge() {
-  const randomBytes = crypto.randomBytes(32);
-  return base64url.encode(randomBytes);
-}
